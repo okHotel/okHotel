1d9e72ff7c020b847c70cccc83b3caa634727066
VariniChiara <chiara.varini@studio.unibo.it>
2018-07-13 16:09:51 +0200

    move home into app directory

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './costumer/registration/registration.component';
 import { MenuComponent } from './menu/menu.component';
 import { MenuVariationsComponent } from './menu-variations/menu-variations.component';
-import { HomeComponent } from './costumer/home/home.component';
+import { HomeComponent } from './home/home.component';
 import {CustomerDetailComponent} from './costumer/customer-detail/customer-detail.component';
 import {FormsModule} from '@angular/forms';
 import { ProfileComponent } from './profile/profile.component';
