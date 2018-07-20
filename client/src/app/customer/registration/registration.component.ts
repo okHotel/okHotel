0999bd7dc53ad1f98237e1e7e1adb121a8ff6b2b
federicapecci <federica.pecci2@studio.unibo.it>
2018-07-20 23:22:08 +0200

    connection between registration and db

--- client/src/app/customer/registration/registration.component.ts
+++ client/src/app/customer/registration/registration.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { Customer } from '../../customer';
 import { CustomerService } from '../../service/customer.service';
 import {NgForm} from '@angular/forms';
+import {log} from 'util';
 
 @Component({
   selector: 'app-registration',
@@ -11,7 +12,19 @@ import {NgForm} from '@angular/forms';
 })
 export class RegistrationComponent implements OnInit {
 
-  private customer: Customer;
+    @Input() customer: Customer;
+    roomsNumber = [102,103,201,204];
+    peopleNumber = 3;
+    @Input() confirmPassword: string;
+
+    private reservation = {
+        "start": "10/06/2018",
+        "end": "20/06/2018",
+        "roomNumber": 102,
+        "numberOfPeople": 2,
+        "bookingName": "Raffaella",
+        "bookingSurname": "Carrà"
+    }
 
   constructor(
       private customerService: CustomerService,
@@ -31,16 +44,36 @@ export class RegistrationComponent implements OnInit {
     }
 
    public onSubmit() {
-        this.customerService.addCustomer(this.customer).subscribe(
-            response => {
-                if (response.success === true) {
-                    // Code smell here!
-                    console.log('Customer added!');
-                } else {
-                    console.log(response);
-                }
-            }
-        );
-       this.router.navigateByUrl('/home' );
-    }
+
+
+        log(this.confirmPassword, this.customer.password,
+            this.customer.bookingName, this.reservation.bookingName,
+            this.customer.bookingSurname, this.reservation.bookingSurname,
+            this.customer.roomNumber, this.reservation.roomNumber);
+
+       if (this.isInputValid()) {
+
+           this.customerService.addCustomer(this.customer).subscribe(
+               response => {
+                   if (response.success === true) {
+                       // Code smell here!
+                       console.log('Customer added!');
+                   } else {
+                       console.log(response);
+                   }
+               }
+           );
+           this.router.navigateByUrl('');
+
+       }
+   }
+
+   private isInputValid() {
+        console.log("isInputValid");
+       return this.confirmPassword == this.customer.password &&
+           this.customer.bookingName == this.reservation.bookingName &&
+           this.customer.bookingSurname == this.reservation.bookingSurname &&
+           this.customer.roomNumber == this.reservation.roomNumber;
+
+   }
 }
