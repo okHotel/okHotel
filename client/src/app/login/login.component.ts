a3401b7f1a517ff8f7b8465600cf5c53c3d1f8ec
federicapecci <federica.pecci2@studio.unibo.it>
2018-08-23 01:32:36 +0200

    login css done

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { CustomerService } from '../service/customer/customer.service';
-import { HttpErrorResponse } from '@angular/common/http';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {CustomerService} from '../service/customer/customer.service';
+import {HttpErrorResponse} from '@angular/common/http';
 import {ActivatedRoute, Router} from '@angular/router';
 import {AuthService} from '../service/auth/auth.service';
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
-    styleUrls: ['./login.component.css']
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
     isLoginError : boolean = false;
