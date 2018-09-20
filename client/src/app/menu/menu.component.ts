12c8cc2e850399562a487da89f038569652625f0
VariniChiara <chiara.varini@studio.unibo.it>
2018-09-20 11:13:36 +0200

    fix color theme

--- client/src/app/menu/menu.component.ts
+++ client/src/app/menu/menu.component.ts
@@ -37,17 +37,16 @@ export class MenuComponent implements OnInit {
               public themingService: ThemingService) {
     if (this.themingService.isUseBackgroundOn()) {
       document.body.style.backgroundImage = "url('../../assets/images/restaurant.jpg')";
-      document.body.style.backgroundRepeat = "repeat";
-      document.body.style.backgroundSize = "cover";
-      document.body.style.backgroundPosition = "center center";
+      document.body.style.backgroundRepeat = 'repeat';
+      document.body.style.backgroundSize = 'cover';
+      document.body.style.backgroundPosition = 'center center';
     }
-    
   }
 
   get myStyle() {
     return {
       'width': (this.menu.showLunchVariations || this.menu.showDinnerVariations) ? '40%' : '0',
-      'padding': (this.menu.showLunchVariations || this.menu.showDinnerVariations) ? '5%' : '0',
+      'padding-top': (this.menu.showLunchVariations || this.menu.showDinnerVariations) ? '5%' : '0',
       'transition': '0.5s'
     };
   }
